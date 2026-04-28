faf79ef8edc536b9d4f055362183acfdd9c8c6b6
AhmedAbayazid000 <ak.abayazid@gmail.com>
2026-04-29 00:36:46 +0200

    Classes

--- Array and vectors/main.cpp
+++ Array and vectors/main.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <ctime>
+class bank {
+private:
+    int id_;
+    float balance_;
+public:
+    void set_id(int id){
+        id_=id;
+    }
+    void set_balance(float balance){
+        balance_=balance;
+    }
+    int get_id(){
+        return id_;
+    }
+    int idgen(){
+        int min=100000;
+        int max = 999999;
+        srand(time(nullptr));
+        id_= rand () % (min+max)-min;    
+        return id_;
+    }
+    float withdraw(){
+        float amount;
+        std::cin>>amount;
+        if (amount<balance_){
+            std::cout<<"Insufficent balance"<<std::endl;
+        }
+        else {
+            balance_= -amount;
+        }
+        return balance_;
+    }
+    int deposit (){
+        int amount; 
+        if (amount%5!=0 ){
+            std::cout<<"Cannot deposit this amount"<<std::endl;
+        }
+        else {
+            balance_= +amount;
+        }
+     return balance_;
+    }
+    float get_balance(){
+        return balance_;
+    }
+    void final_balance(){
+      get_balance;  
+    }
 
+};
 
-int main()
-{
-   std::cout << "Hello World!" << std::endl;
-   std::cout << "This is a C++17 project." << std::endl;
-    return 0;
-}
-
+// int main(){
+//     std::cout<<"OK"<<std::endl;
+// return 0;
+// }
